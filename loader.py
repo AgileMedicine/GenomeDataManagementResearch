775dbd19069e054799538fcadaf791c35b106455
Wade Schulz <wade.schulz@gmail.com>
2014-01-04 01:17:33 -0600

    add multi-chromosome support, bug fixes

--- loader.py
+++ loader.py
@@ -3,9 +3,16 @@ import mysql.connector, pymongo
 from mysql.connector import errorcode
 from pymongo import MongoClient
 
+# Open results file
+resultsFile = open('results.txt', 'w')
+
 # Data files
-snpFilePath = 'snpData.txt'
-lociFilePath = 'lociData.txt'
+snpFilePath = 'snpData-chr{0}.txt'
+lociFilePath = 'lociData-chr{0}.txt'
+
+# Chromosome list
+chromosomes = ["21"] # dev list
+# chromosomes = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","X","Y","MT"] # complete list
 
 # Connection strings
 mysqlConfig = {
@@ -43,7 +50,7 @@ TABLES['snp'] = (
     "CREATE TABLE IF NOT EXISTS `snp`("
     "  `id` int(11) NOT NULL AUTO_INCREMENT,"
     "  `rsid` varchar(45) NOT NULL,"
-    "  `chr` varchar(5) NOT NULL"
+    "  `chr` varchar(5) NOT NULL,"
     "  `has_sig` binary(1) NOT NULL,"
     "  PRIMARY KEY (`id`)"
     ") ENGINE=InnoDB AUTO_INCREMENT=862719 DEFAULT CHARSET=utf8;")
@@ -57,10 +64,10 @@ TABLES['locus'] = (
     "  PRIMARY KEY (`id`),"
     "  KEY `idx_snp_idx` (`snp_id`),"
     "  CONSTRAINT `idx_snp` FOREIGN KEY (`snp_id`) REFERENCES `snp` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION"
-    ") ENGINE=InnoDB AUTO_INCREMENT=7564 DEFAULT CHARSET=utf8;)")
+    ") ENGINE=InnoDB AUTO_INCREMENT=7564 DEFAULT CHARSET=utf8;")
 
 for name, ddl in TABLES.iteritems():
-    createDbCursor.execute(ddl,multi=True)
+    createDbCursor.execute(ddl)
     mysqlConnection.commit()
 
 createDbCursor.close()
@@ -72,98 +79,116 @@ rsidList = {}      # Dictionary of RSIDs that will also hold the
                    # primary key for each SNP in SQL
 documents = {}     # Dictionary for MongoDB SNP/loci documents
 
-# Read in data from SNP file
-with open(snpFilePath,'r') as csvfile:
-    data = csv.reader(csvfile,delimiter='\t')
-    for row in data:
-        if(len(row) == 3):        
-            hasSig = False
-            if row[2] != '' and row[2] != 'untested':
-                hasSig = True
-            rsidList[row[0]] = 0
-            insStr = "INSERT INTO snp (rsid, chr, has_sig) VALUES (\"{0}\", {1}, {2})".format(row[0], row[1], hasSig)
-            snpInserts[row[0]] = insStr
-            documents[row[0]] = {"rsid":row[0], "chr":row[1], "has_sig":row[2], "loci":[]}
-
-# Insert SNP data into MySQL
-mysqlCursor = mysqlConnection.cursor()
-
-# Log current run start time and number of SNPs
-snpEntries = len(snpInserts)
-start = time.time()
-
-# For each snp, insert record and then grab primary key
-for rsid,snp in snpInserts.iteritems():
-    mysqlCursor.execute(snp)
-    rsidList[rsid] = mysqlCursor.lastrowid
+for curChr in chromosomes:
+    print "Chromosome " + str(curChr)
     
-# Commit all inserts to MySQL and grab end time
-mysqlConnection.commit()
-
-# Log completed time, close MySQL cursor
-end=time.time()
-mysqlSnpTime = end-start
-mysqlCursor.close()
-
-print "SNPs: " + str(mysqlSnpTime) + "s (" + str(snpEntries) + " records)"
-
-# Clear list of SNPs to free up memory
-snpInserts.clear()
-
-# Now that we have primary keys for each SNP, read in loci data
-with open(lociFilePath,'r') as csvfile:
-    data = csv.reader(csvfile,delimiter='\t')
-    for row in data:
-        if(len(row) == 4):
-            if row[0] in rsidList and rsidList[row[0]] > 0:
-                insStr = "INSERT INTO locus (mrna_acc, gene, class, snp_id) VALUES (\"{0}\", \"{1}\", \"{2}\", {3})".format(row[1], row[2], row[3], rsidList[row[0]])
-                lociInserts.append(insStr)
-                curDoc = documents[row[0]]
-                if curDoc["loci"] is None:
-                    curDoc["loci"] = [{"mrna_acc":row[1],"gene":row[2],"class":row[3]}]
-                else:
-                    curDoc["loci"] = curDoc["loci"].append({"mrna_acc":row[1],"gene":row[2],"class":row[3]})
-                documents[row[0]] = curDoc
-
-# Create new cursor, enter loci data into MySQL
-cursor = mysqlConnection.cursor()
-
-# Log current run start time and number of loci
-lociEntries = len(lociInserts)
-start = time.time()
-
-# Insert each locus
-for locus in lociInserts:
-    cursor.execute(locus)
-
-# Commit data to MySQL
-mysqlConnection.commit()
-
-# Log end time and total MySQL time
-end=time.time()
-
-mysqlLociTime = end-start
-print "Loci: " + str(mysqlLociTime) + "s (" + str(lociEntries) + " records)"
-
-mysqlTotalTime = mysqlSnpTime + mysqlLociTime
-print "Total MySQL time: " + str(mysqlTotalTime) + "s"
-
-# Close MySQL cursor
-cursor.close()
-
-# Log start time for MongoDB inserts
-start = time.time()
-mongoDocuments = len(documents)
-
-# Insert each document with SNP and loci data
-for v in documents.iteritems():
-    mongoCollection.insert(v[1])
+    # Set file paths for current chromosome
+    curSnpFilePath = snpFilePath.format(curChr)
+    curLociFilePath = lociFilePath.format(curChr)
     
-# Log end time
-end=time.time()
+    # Clear dictionaries
+    snpInserts.clear()
+    lociInserts = []
+    rsidList.clear()
+    documents.clear()
+    
+    # Read in data from SNP file
+    with open(curSnpFilePath,'r') as csvfile:
+        data = csv.reader(csvfile,delimiter='\t')
+        for row in data:
+            if(len(row) == 3):        
+                hasSig = False
+                if row[2] != '' and row[2] != 'untested':
+                    hasSig = True
+                rsidList[row[0]] = 0
+                insStr = "INSERT INTO snp (rsid, chr, has_sig) VALUES (\"{0}\", {1}, {2})".format(row[0], row[1], hasSig)
+                snpInserts[row[0]] = insStr
+                documents[row[0]] = {"rsid":row[0], "chr":row[1], "has_sig":row[2], "loci":[]}
+    
+    # Insert SNP data into MySQL
+    mysqlCursor = mysqlConnection.cursor()
+    
+    # Log current run start time and number of SNPs
+    snpEntries = len(snpInserts)
+    start = time.time()
+    
+    # For each snp, insert record and then grab primary key
+    for rsid,snp in snpInserts.iteritems():
+        mysqlCursor.execute(snp)
+        rsidList[rsid] = mysqlCursor.lastrowid
+        
+    # Commit all inserts to MySQL and grab end time
+    mysqlConnection.commit()
+    
+    # Log completed time, close MySQL cursor
+    end=time.time()
+    mysqlSnpTime = end-start
+    mysqlCursor.close()
+    
+    print "\tSNPs: " + str(mysqlSnpTime) + "s (" + str(snpEntries) + " records)"
+    
+    # Clear list of SNPs to free up memory
+    snpInserts.clear()
+    
+    # Now that we have primary keys for each SNP, read in loci data
+    with open(curLociFilePath,'r') as csvfile:
+        data = csv.reader(csvfile,delimiter='\t')
+        for row in data:
+            if(len(row) == 4):
+                if row[0] in rsidList and rsidList[row[0]] > 0:
+                    insStr = "INSERT INTO locus (mrna_acc, gene, class, snp_id) VALUES (\"{0}\", \"{1}\", \"{2}\", {3})".format(row[1], row[2], row[3], rsidList[row[0]])
+                    lociInserts.append(insStr)
+                    curDoc = documents[row[0]]
+                    if curDoc["loci"] is None:
+                        curDoc["loci"] = [{"mrna_acc":row[1],"gene":row[2],"class":row[3]}]
+                    else:
+                        curDoc["loci"] = curDoc["loci"].append({"mrna_acc":row[1],"gene":row[2],"class":row[3]})
+                    documents[row[0]] = curDoc
+    
+    # Create new cursor, enter loci data into MySQL
+    cursor = mysqlConnection.cursor()
+    
+    # Log current run start time and number of loci
+    lociEntries = len(lociInserts)
+    start = time.time()
+    
+    # Insert each locus
+    for locus in lociInserts:
+        cursor.execute(locus)
+    
+    # Commit data to MySQL
+    mysqlConnection.commit()
+    
+    # Log end time and total MySQL time
+    end=time.time()
+    
+    mysqlLociTime = end-start
+    print "\tLoci: " + str(mysqlLociTime) + "s (" + str(lociEntries) + " records)"
+    
+    mysqlTotalTime = mysqlSnpTime + mysqlLociTime
+    print "\t\tTotal MySQL time: " + str(mysqlTotalTime) + "s"
+    
+    # Close MySQL cursor
+    cursor.close()
+    
+    # Log start time for MongoDB inserts
+    start = time.time()
+    mongoDocuments = len(documents)
+    
+    # Insert each document with SNP and loci data
+    for v in documents.iteritems():
+        mongoCollection.insert(v[1])
+        
+    # Log end time
+    end=time.time()
+    
+    mongoTime = end-start
+    print "\tMongoDB: " + str(mongoTime) + "s (" + str(mongoDocuments) +" documents)"
 
-mongoTime = end-start
-print "MongoDB: " + str(mongoTime) + "s (" + str(mongoDocuments) +" documents)"
+    results = curChr + "\t" + str(mysqlSnpTime) + "\t" + str(snpEntries) + "\t" + str(mysqlLociTime) + "\t" + str(lociEntries) + "\t" + str(mongoTime) + "\t" + str(mongoDocuments)
+    print results
+    resultsFile.write(results)
 
+resultsFile.close()
 mysqlConnection.close()
 mongoClient.close()
